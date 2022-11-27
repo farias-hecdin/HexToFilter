3447f91ad68d33454b0a2b72e9e1084bbfb20f65
Hecdin Farias <contact@hecdin.com>
2022-11-27 08:56:26 -0400

    Update v1.5.1

--- ASSETS/scripts/features.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const copyText = function () {
-  let textBox = document.getElementById("TEXT_INPUT");
-  textBox.select();
-  document.execCommand("copy");
-};
--- src/scripts/converter.js
+++ src/scripts/converter.js
@@ -4,22 +4,22 @@ class Color {
   constructor(r, g, b) {
     this.set(r, g, b);
   }
-
+  
   toString() {
     return `rgb(${Math.round(this.r)}, ${Math.round(this.g)}, ${Math.round(this.b)})`;
   }
-
+  
   set(r, g, b) {
     this.r = this.clamp(r);
     this.g = this.clamp(g);
     this.b = this.clamp(b);
   }
-
+  
   hueRotate(angle = 0) {
     angle = (angle / 180) * Math.PI;
     const sin = Math.sin(angle);
     const cos = Math.cos(angle);
-
+    
     this.multiply([
       0.213 + cos * 0.787 - sin * 0.213,
       0.715 - cos * 0.715 - sin * 0.715,
@@ -32,7 +32,7 @@ class Color {
       0.072 + cos * 0.928 + sin * 0.072,
     ]);
   }
-
+  
   grayscale(value = 1) {
     this.multiply([
       0.2126 + 0.7874 * (1 - value),
@@ -46,7 +46,7 @@ class Color {
       0.0722 + 0.9278 * (1 - value),
     ]);
   }
-
+  
   sepia(value = 1) {
     this.multiply([
       0.393 + 0.607 * (1 - value),
@@ -60,7 +60,7 @@ class Color {
       0.131 + 0.869 * (1 - value),
     ]);
   }
-
+  
   saturate(value = 1) {
     this.multiply([
       0.213 + 0.787 * value,
@@ -74,7 +74,7 @@ class Color {
       0.072 + 0.928 * value,
     ]);
   }
-
+  
   multiply(matrix) {
     const newR = this.clamp(this.r * matrix[0] + this.g * matrix[1] + this.b * matrix[2]);
     const newG = this.clamp(this.r * matrix[3] + this.g * matrix[4] + this.b * matrix[5]);
@@ -83,27 +83,27 @@ class Color {
     this.g = newG;
     this.b = newB;
   }
-
+  
   brightness(value = 1) {
     this.linear(value);
   }
-
+  
   contrast(value = 1) {
     this.linear(value, -(0.5 * value) + 0.5);
   }
-
+  
   linear(slope = 1, intercept = 0) {
     this.r = this.clamp(this.r * slope + intercept * 255);
     this.g = this.clamp(this.g * slope + intercept * 255);
     this.b = this.clamp(this.b * slope + intercept * 255);
   }
-
+  
   invert(value = 1) {
     this.r = this.clamp((value + (this.r / 255) * (1 - 2 * value)) * 255);
     this.g = this.clamp((value + (this.g / 255) * (1 - 2 * value)) * 255);
     this.b = this.clamp((value + (this.b / 255) * (1 - 2 * value)) * 255);
   }
-
+  
   hsl() {
     // Code taken from https://stackoverflow.com/a/9493060/2688027, licensed under CC BY-SA.
     const r = this.r / 255;
@@ -112,15 +112,15 @@ class Color {
     const max = Math.max(r, g, b);
     const min = Math.min(r, g, b);
     let h,
-      s,
-      l = (max + min) / 2;
-
+    s,
+    l = (max + min) / 2;
+    
     if (max === min) {
       h = s = 0;
     } else {
       const d = max - min;
       s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-
+      
       switch (max) {
         case r:
           h = (g - b) / d + (g < b ? 6 : 0);
@@ -134,14 +134,14 @@ class Color {
       }
       h /= 6;
     }
-
+    
     return {
       h: h * 100,
       s: s * 100,
       l: l * 100,
     };
   }
-
+  
   clamp(value) {
     if (value > 255) {
       value = 255;
@@ -152,13 +152,14 @@ class Color {
   }
 }
 
+
 class Solver {
   constructor(target, baseColor) {
     this.target = target;
     this.targetHSL = target.hsl();
     this.reusedColor = new Color(0, 0, 0);
   }
-
+  
   solve() {
     const result = this.solveNarrow(this.solveWide());
     return {
@@ -167,13 +168,13 @@ class Solver {
       filter: this.css(result.values),
     };
   }
-
+  
   solveWide() {
     const A = 5;
     const c = 15;
     const a = [60, 180, 18000, 600, 1.2, 1.2];
-
     let best = { loss: Infinity };
+    
     for (let i = 0; best.loss > 25 && i < 3; i++) {
       const initial = [50, 20, 3750, 50, 100, 100];
       const result = this.spsa(A, a, c, initial, 1000);
@@ -183,7 +184,7 @@ class Solver {
     }
     return best;
   }
-
+  
   solveNarrow(wide) {
     const A = wide.loss;
     const c = 2;
@@ -191,17 +192,16 @@ class Solver {
     const a = [0.25 * A1, 0.25 * A1, A1, 0.25 * A1, 0.2 * A1, 0.2 * A1];
     return this.spsa(A, a, c, wide.values, 500);
   }
-
+  
   spsa(A, a, c, values, iters) {
     const alpha = 1;
     const gamma = 0.16666666666666666;
-
     let best = null;
     let bestLoss = Infinity;
     const deltas = new Array(6);
     const highArgs = new Array(6);
     const lowArgs = new Array(6);
-
+    
     for (let k = 0; k < iters; k++) {
       const ck = c / Math.pow(k + 1, gamma);
       for (let i = 0; i < 6; i++) {
@@ -209,14 +209,14 @@ class Solver {
         highArgs[i] = values[i] + ck * deltas[i];
         lowArgs[i] = values[i] - ck * deltas[i];
       }
-
+      
       const lossDiff = this.loss(highArgs) - this.loss(lowArgs);
       for (let i = 0; i < 6; i++) {
         const g = (lossDiff / (2 * ck)) * deltas[i];
         const ak = a[i] / Math.pow(A + k + 1, alpha);
         values[i] = fix(values[i] - ak * g, i);
       }
-
+      
       const loss = this.loss(values);
       if (loss < bestLoss) {
         best = values.slice(0);
@@ -224,7 +224,7 @@ class Solver {
       }
     }
     return { values: best, loss: bestLoss };
-
+    
     function fix(value, idx) {
       let max = 100;
       if (idx === 2 /* saturate */) {
@@ -232,7 +232,7 @@ class Solver {
       } else if (idx === 4 /* brightness */ || idx === 5 /* contrast */) {
         max = 200;
       }
-
+      
       if (idx === 3 /* hue-rotate */) {
         if (value > max) {
           value %= max;
@@ -247,19 +247,18 @@ class Solver {
       return value;
     }
   }
-
+  
   loss(filters) {
     // Argument is array of percentages.
     const color = this.reusedColor;
     color.set(0, 0, 0);
-
     color.invert(filters[0] / 100);
     color.sepia(filters[1] / 100);
     color.saturate(filters[2] / 100);
     color.hueRotate(filters[3] * 3.6);
     color.brightness(filters[4] / 100);
     color.contrast(filters[5] / 100);
-
+    
     const colorHSL = color.hsl();
     return (
       Math.abs(color.r - this.target.r) +
@@ -268,18 +267,19 @@ class Solver {
       Math.abs(colorHSL.h - this.targetHSL.h) +
       Math.abs(colorHSL.s - this.targetHSL.s) +
       Math.abs(colorHSL.l - this.targetHSL.l)
-    );
+      );
   }
-
+  
   css(filters) {
     function fmt(idx, multiplier = 1) {
       return Math.round(filters[idx] * multiplier);
     }
     return `invert(${fmt(0)}%) sepia(${fmt(1)}%) saturate(${fmt(2)}%) hue-rotate(${fmt(3, 3.6)}deg) brightness(${fmt(
-      4
+    4
     )}%) contrast(${fmt(5)}%)`;
   }
 }
+
 
 function hexToRgb(hex) {
   // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
@@ -287,10 +287,11 @@ function hexToRgb(hex) {
   hex = hex.replace(shorthandRegex, (m, r, g, b) => {
     return r + r + g + g + b + b;
   });
-
+  
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null;
 }
+
 
 function ready(fn) {
   if (document.readyState != "loading") {
@@ -301,38 +302,35 @@ function ready(fn) {
 }
 
 ready(() => {
-  document.getElementById("BTN_EXECUTE").addEventListener("click", () => {
-    const rgbValue = document.getElementById("TARGET_HEX").value;
-    const rgbColor = hexToRgb(rgbValue);
-
-    if (rgbColor.length !== 3) {
-      alert("Invalid format!");
-      return;
-    }
-
-    const color = new Color(rgbColor[0], rgbColor[1], rgbColor[2]);
-    const solver = new Solver(color);
-    const result = solver.solve();
-    let lossMsg;
-
-    /*
-    if (result.loss < 1) {
-      lossMsg = "This is a perfect result.";
-    } else if (result.loss < 5) {
-      lossMsg = "The is close enough.";
-    } else {
-      lossMsg = "The color is extremely off. Run it again!";
-    }
-    */
-
-    let hexColor = color.toString();
-    let filterColor = result.filter;
-
-    document.getElementById("HEX_PIXEL").style.backgroundColor = hexColor;
-    document.getElementById("FILTER_PIXEL").style.filter = filterColor;
-    document.getElementById("TEXT_INPUT").value = `filter: ${result.filter};`;
-
-    // document.querySelectorAll(".s-lossDetail")[0].innerHTML = `${result.loss.toFixed(1)}% <span> ${lossMsg} </span>`;
-    document.querySelectorAll(".s-lossDetail")[0].innerHTML = `${result.loss.toFixed(1)}%`;
-  });
+  document.getElementById("id_execute").addEventListener("click", hexToFilter);
+  document.getElementById("id_copy").addEventListener("click", copyText);
 });
+
+
+const hexToFilter = function() {
+  const rgbValue = document.getElementById("id_targetHex").value;
+  const rgbColor = hexToRgb(rgbValue);
+  
+  if (rgbColor.length !== 3) {
+    alert("Invalid format!");
+    return;
+  }
+  const color = new Color(rgbColor[0], rgbColor[1], rgbColor[2]);
+  const solver = new Solver(color);
+  const result = solver.solve();
+  let lossMsg;
+  let hexColor = color.toString();
+  let filterColor = result.filter;
+  
+  document.getElementById("id_hexPixel").style.backgroundColor = hexColor;
+  document.getElementById("id_filterPixel").style.filter = filterColor;
+  document.getElementById("id_textInput").value = `filter: ${result.filter};`;
+  document.getElementById("id_lossDetail").textContent = `${result.loss.toFixed(1)}%`;
+}
+
+
+const copyText = function() {
+  let textBox = document.getElementById("id_textInput");
+  textBox.select();
+  document.execCommand("copy");
+};
